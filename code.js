cf967f2f8baac244ef6159042393dc1f31827ec1
idiotkrivorukiy3456 <pisuuvokno777hour@gmail.com>
2025-10-07 20:01:43 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,9 @@
+// Функция для скрытия/показа содержимого разделов
+      function toggleContent(contentId) {
+          const content = document.getElementById(contentId);
+          content.classList.toggle('show');
+          
+          // Обновляем текст заголовка    
+             
+          }
+      
